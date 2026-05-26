52778a6e0868f5abc7f076bb4a4f4532f6818e66
Joshua Granick <jgranick@users.noreply.github.com>
2026-05-26 11:50:16 -0700

    signals: Add more tests

--- packages/signals/src/signal.test.ts
+++ packages/signals/src/signal.test.ts
@@ -1,8 +1,18 @@
-import { createSignal } from './signal';
+import { createSignal, noop } from './signal';
 
 describe('createSignal', () => {
   it('initializes with data=null', () => {
     const signal = createSignal<() => void>();
     expect(signal.data).toBeNull();
+  });
+});
+
+describe('noop', () => {
+  it('is a function', () => {
+    expect(typeof noop).toBe('function');
+  });
+
+  it('returns undefined', () => {
+    expect(noop()).toBeUndefined();
   });
 });
