e6b5f3daad80351e3075804c22a1c1d43a89f12e
Joshua Granick <jgranick@users.noreply.github.com>
2026-05-26 16:29:39 -0700

    all: Update vitest setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,8 +1,10 @@
+export {};
+
 if (typeof window !== 'undefined' && 'document' in window) {
   // jsdom / browser environment
   // @ts-expect-error: quiet warning about types
-  import('@testing-library/jest-dom');
-  import('vitest-webgl-canvas-mock');
+  await import('@testing-library/jest-dom');
+  await import('vitest-webgl-canvas-mock');
 } else {
   // node environment
 }
