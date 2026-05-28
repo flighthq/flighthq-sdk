5b616360d9883aa5f99e986b80c20faff926b24f
Joshua Granick <jgranick@users.noreply.github.com>
2026-05-28 07:39:43 -0700

    all: Vitest config fix

--- vitest.config.base.ts
+++ vitest.config.base.ts
@@ -3,7 +3,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths({ root: __dirname })],
   test: {
     globals: true,
     setupFiles: [path.resolve(__dirname, 'vitest.setup.ts')],
